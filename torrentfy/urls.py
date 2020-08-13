16d8e5754f7f2cd6ada1cc033519a6230b056b9a
AlanJaeger <aland295@gmail.com>
2020-08-13 20:26:15 -0300

    criado os cadastros de usuario

--- torrentfy/urls.py
+++ torrentfy/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path
-from torrentfy.views import DashboardView, TelaCadastroView
+from torrentfy.views import DashboardView, TelaCadastroView, CadastroView
 
 
 urlpatterns = [
     url(r'^$', DashboardView.as_view(), name='dashboard'),
     url(r'^cadastro/$', TelaCadastroView.as_view(), name='cadastro'),
-
+    url(r'^registro/$', CadastroView.as_view(), name="registrar")
 ]
--- torrentfy/views.py
+++ torrentfy/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
-
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+from torrentfy.forms import SignupForm
 
 
 class DashboardView(View):
@@ -12,3 +15,9 @@ class DashboardView(View):
 class TelaCadastroView(View):
     def get(self, request):
         return render(request, 'inicio/telacadastro.html')
+
+
+class CadastroView(generic.CreateView):
+        form_class = SignupForm
+        success_url = reverse_lazy('dashboard')
+        template_name = 'inicio/telacadastro.html'
